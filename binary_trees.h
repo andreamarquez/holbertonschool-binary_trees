7cfa37d9e41f094c46efad835a15a3cfa99e5c0c
andreamarquez <andreamarquezg.95@gmail.com>
2024-12-10 14:31:52 +0100

    rename struct

--- binary_trees.h
+++ binary_trees.h
@@ -5,14 +5,14 @@
 /**
  * struct binary_tree_s - Binary tree node
  *
- * @value: Integer stored in the node
+ * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
 struct binary_tree_s
 {
-	int value;
+	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
@@ -22,6 +22,7 @@ typedef struct binary_tree_s binary_tree_t;
 
 /* PROTOTYPES */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 /**
  * binary_tree_print - Prints a binary tree
