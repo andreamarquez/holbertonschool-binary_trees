4bbfa46508d2a3e79fb518443e6f7ff8a31fe296
andreamarquez <andreamarquezg.95@gmail.com>
2024-12-10 13:50:13 +0100

    binary_tree_node

---
+++ binary_trees.h
@@ -0,0 +1,27 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* DATA STRUCTURE */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @value: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int value;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* PROTOTYPES */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);  /* Add this line */
+
+#endif /* BINARY_TREES_H */
